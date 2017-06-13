fcc2bd4e4f3aad6cec999c11ffffb03349f1bc50
Walker Ward <walker.ward1@gmail.com>
2017-06-13 12:06:46 -0700

    deployment $npm_packager_version

--- getModel/index.js
+++ getModel/index.js
@@ -1,34 +1,36 @@
-// /**
-//  * Responds to an HTTP request using data from the request body parsed according
-//  * to the "content-type" header.
-//  *
-//  * @param {Object} req Cloud Function request context.
-//  * @param {Object} res Cloud Function response context.
-//  */
-// exports.getModel = function getModel(req, res) {
-//     let name;
-//
-//     switch (req.get('content-type')) {
-//       // '{"name":"John"}'
-//       case 'application/json':
-//         name = req.body.name;
-//         break;
-//
-//         // 'John', stored in a Buffer
-//       case 'application/octet-stream':
-//         name = req.body.toString(); // Convert buffer to a string
-//         break;
-//
-//         // 'John'
-//       case 'text/plain':
-//         name = req.body;
-//         break;
-//
-//         // 'name=John'
-//       case 'application/x-www-form-urlencoded':
-//         name = req.body.name;
-//         break;
-//     }
-//
-//     res.status(200).send(`Hello ${name || 'World'}!`);
-exports.getModel = (req, res) => res.send("Hello, World!");
+/**
+ * Responds to an HTTP request using data from the request body parsed according
+ * to the "content-type" header.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.getModel = function getModel(req, res) {
+    let name;
+
+    switch (req.get('content-type')) {
+      // '{"name":"John"}'
+      case 'application/json':
+        name = req.body.name;
+        break;
+
+        // 'John', stored in a Buffer
+      case 'application/octet-stream':
+        name = req.body.toString(); // Convert buffer to a string
+        break;
+
+        // 'John'
+      case 'text/plain':
+        name = req.body;
+        break;
+
+        // 'name=John'
+      case 'application/x-www-form-urlencoded':
+        name = req.body.name;
+        break;
+    }
+
+    res.status(200).send(`Hello ${name || 'World'}!`);
+
+};
+// exports.getModel = (req, res) => res.send("Hello, World!");
